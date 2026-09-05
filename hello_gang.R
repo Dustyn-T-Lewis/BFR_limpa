62f0a5607e4d5dd30be8db875b7d87283169ba2a
Tahran Gotla <tahran0806@gamil.com>
2026-09-05 12:12:46 -0500

    Add hello_gang practice script

---
+++ hello_gang.R
@@ -0,0 +1,9 @@
+# hello_gang.R
+# My first commit to the BFR_limpa team repo
+
+greeting <- "Hello, gang! Tahran is on the project."
+print(greeting)
+
+# quick sanity check that R runs
+print(2 + 2)
+
